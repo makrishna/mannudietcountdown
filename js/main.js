05a3a323f7cc85ed651205d0a71931f319f32941
Krishna <macharyakc@gmail.com>
2024-09-29 21:01:01 +1000

    Date change due to various situations

--- js/main.js
+++ js/main.js
@@ -4,4 +4,6 @@ else{if($(input).val().trim()==''){return false;}}}
 function showValidate(input){var thisAlert=$(input).parent();$(thisAlert).addClass('alert-validate');}
 function hideValidate(input){var thisAlert=$(input).parent();$(thisAlert).removeClass('alert-validate');}
 $('.simpleslide100').each(function(){var delay=7000;var speed=1000;var itemSlide=$(this).find('.simpleslide100-item');var nowSlide=0;$(itemSlide).hide();$(itemSlide[nowSlide]).show();nowSlide++;if(nowSlide>=itemSlide.length){nowSlide=0;}
-setInterval(function(){$(itemSlide).fadeOut(speed);$(itemSlide[nowSlide]).fadeIn(speed);nowSlide++;if(nowSlide>=itemSlide.length){nowSlide=0;}},delay);});})(jQuery);
+    setInterval(function () { $(itemSlide).fadeOut(speed); $(itemSlide[nowSlide]).fadeIn(speed); nowSlide++; if (nowSlide >= itemSlide.length) { nowSlide = 0; } }, delay);
+});
+})(jQuery);
